b4e9b28d91dc0a32605ee8f00b6d9a757ab3c6b0
M-kip <bettmoseskiprono@gmail.com>
2026-09-25 00:31:10 +0300

    fix: clean up vite.config.js by removing commented proxy configuration and adjusting indentation

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,6 @@ import frappeui from "frappe-ui/vite";
 import path from "path";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
 	define: {
 		__VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
@@ -11,7 +10,7 @@ export default defineConfig({
 	plugins: [
 		frappeui({
 			siteName: "flairdentalcare.localhost",
-      siteList: ["flairdentalcare.localhost"],
+			siteList: ["flairdentalcare.localhost"],
 			frontendRoute: "/app",
 			frappeProxy: {
 				port: 8000,
@@ -39,16 +38,8 @@ export default defineConfig({
 	},
 	server: {
 		port: 8080,
-    strictPort: true, // 👈 Ensure it enforces port 8080
+		strictPort: true,
 		allowedHosts: true,
-		proxy: {
-			"^/(?!(?:frontend|app|desk|login|api|assets|files|private|pages|src|node_modules)(?:[/?#]|$)|@|__)(?![^?]*\\.)[^/?#].*":
-				{
-					target: `http://127.0.0.1:${process.env.FRAPPE_WEB_SERVER_PORT || 8000}`,
-					router: (req) =>
-						`http://${req.headers.host.split(":")[0]}:${process.env.FRAPPE_WEB_SERVER_PORT || 8000}`,
-				},
-		},
 	},
 	optimizeDeps: {
 		include: ["frappe-ui > feather-icons", "engine.io-client", "interactjs", "highlight.js/lib/core"],
